2ce3e0dc3934dd1d58e23e520218bf4c681009b8
kojima rikuto <kojikoji7765@icloud.com>
2022-07-11 00:15:42 +0900

    Initial Commit

---
+++ HorseWeather/HorseWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorseWeatherApp.swift
+//  HorseWeather
+//
+//  Created by 児島　陸斗 on 2022/07/11.
+//
+
+import SwiftUI
+
+@main
+struct HorseWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
